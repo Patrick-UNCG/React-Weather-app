bca8fbdb4690b58eba05e0a429e332453d748a07
Patrick Ksor <71914119+Patrick-UNCG@users.noreply.github.com>
2022-12-18 20:25:54 -0500

    added new backgrounds

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import sunny from "./images/sunny.jpg"
 import night from "./images/night.jpg"
 import cloudyDay from "./images/cloudy.jpg"
 import cloudyNight from "./images/cloudynight.webp"
+import rainyDay from "./images/rainy.jpg"
+import rainyNight from "./images/rainyNight.webp"
+import snowy from "./images/snowy.jpg"
 import { motion } from "framer-motion"
 
 function App() {
@@ -10,6 +13,8 @@ function App() {
   const [city, setCity] = React.useState("London")
   const [isError, setIsError] = React.useState(true)
   const [isDay, setIsDay] = React.useState(0)
+  const [isCel, setIsCel] = React.useState(false)
+  const [isDarkMode, setIsDarkMode] = React.useState(false)
   let background = night;
   
   
@@ -59,7 +64,16 @@ function App() {
           background=cloudyDay
           break
         }
-      default: background = sunny
+      case ((x>=1063 && x<=1201) || (x>=1240 && x<=1276)):
+        if(isDay===0){
+          background=rainyNight
+          break
+        }
+        else{
+          background=rainyDay
+          break
+        }
+      default: background = snowy
     }
   }
   if(weatherData){
@@ -70,6 +84,18 @@ function App() {
     e.preventDefault()
     setCity(cityInput.value)
     console.log(isDay)
+    if(isCel){
+      changeMeasurement()
+    }
+  }
+
+  function changeLightMode(){
+    setIsDarkMode(!isDarkMode)
+  }
+
+  function changeMeasurement(){
+    setIsCel(!isCel);
+    console.log(isCel)
   }
 
   function getForecastDay(date){
@@ -111,12 +137,17 @@ function App() {
       key={city}
       initial={{ opacity: 0, y: -500 }}
       animate={{ opacity: 1, y: 0 }}
-       className="App-header">
+       className="App-header" style={{color: isDarkMode ? "white":"black", background: isDarkMode?"gray" : "white"}}>
         <div className="weather-data">
           <p className="city-name">{weatherData.location.name}, {weatherData.location.region}, {weatherData.location.country}</p>
+          <label class="switch">
+            <input type="checkbox" onClick={changeMeasurement}/>
+            <span class="slider round"></span>
+          </label>
+          {isDarkMode ? <i onClick={changeLightMode} class="fa-solid fa-lightbulb"></i>:<i onClick={changeLightMode} class="fa-regular fa-lightbulb"></i>}
           <div className="weather-data-current-temp-div">
             <img className='weather-icon' src= {weatherData.current.condition.icon} alt="current weather icon"/>
-            <h1 className='weather-temp'>{Math.round(weatherData.current.temp_f)}°F</h1>
+            <h1 className='weather-temp'>{!isCel ? Math.round(weatherData.current.temp_f) + "°F" : Math.round(weatherData.current.temp_c) + "°C"} </h1>
           </div>
           <h2 className="weather-data-condition">{weatherData.current.condition.text}</h2>
           <div className="weather-data-sub-info">
